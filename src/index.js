0c876f562a9152bc726f5083e802b9cf969ccb20
Karthik P <karthik3904@gmail.com>
2025-07-22 12:43:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  
-  body {
-    background-color: #f0f2f5;
-  }
-  
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
